0a170d95d6b619d4353360b08a2e18b182216a59
Olawale Onabola <walexi4great@gmail.com>
2025-01-08 07:20:44 -0800

    setup

--- model.py
+++ model.py
@@ -1,24 +1,27 @@
 from flax import linen as nn
 
-class CNN(nn.Module):
-  """A simple CNN model."""
-  is_training: bool = True
-  img_size : int = 32
-  num_classes: int = 10
 
-  @nn.compact
-  def __call__(self, x):
-    x = nn.Conv(features=self.img_size, kernel_size=(3, 3))(x)
-    x = nn.relu(x)
-    x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
-    x = nn.Conv(features=64, kernel_size=(3, 3))(x)
-    x = nn.relu(x)
-    x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
-    x = x.reshape((x.shape[0], -1))  # flatten
-    x = nn.Dense(features=256)(x)
-    x = nn.relu(x)
-    x = nn.Dense(features=self.num_classes)(x)
-    return x
+class CNN(nn.Module):
+    """A simple CNN model."""
+
+    is_training: bool = True
+    img_size: int = 32
+    num_classes: int = 10
+
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Conv(features=self.img_size, kernel_size=(3, 3))(x)
+        x = nn.relu(x)
+        x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
+        x = nn.Conv(features=64, kernel_size=(3, 3))(x)
+        x = nn.relu(x)
+        x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
+        x = x.reshape((x.shape[0], -1))  # flatten
+        x = nn.Dense(features=256)(x)
+        x = nn.relu(x)
+        x = nn.Dense(features=self.num_classes)(x)
+        return x
+
 
 def create_model(img_size, num_classes):
     return CNN(img_size=img_size, num_classes=num_classes)
--- model_test.py
+++ model_test.py
@@ -4,24 +4,26 @@ import jax
 from jax import numpy as jnp
 from model import create_model
 
+jax.config.update("jax_disable_most_optimizations", True)
 
-jax.config.update('jax_disable_most_optimizations', True)
 
 class ModelTest(parameterized.TestCase):
-  """Test cases for CNN model definition."""
+    """Test cases for CNN model definition."""
 
-  @parameterized.product(img_size=[28, 32, 64], num_classes=[10, 20], n_channels=[1, 3])
-  def test_model(self , img_size, num_classes, n_channels):
-    """Tests CNN model definition and output (variables)."""
-    model = create_model(img_size, num_classes)
-    inputs =  jnp.ones((1, img_size, img_size, n_channels))
-    variables = model.init(jax.random.key(0), inputs)
-    out = model.apply(variables, inputs)
+    @parameterized.product(
+        img_size=[28, 32, 64], num_classes=[10, 20], n_channels=[1, 3]
+    )
+    def test_model(self, img_size, num_classes, n_channels):
+        """Tests CNN model definition and output (variables)."""
+        model = create_model(img_size, num_classes)
+        inputs = jnp.ones((1, img_size, img_size, n_channels))
+        variables = model.init(jax.random.key(0), inputs)
+        out = model.apply(variables, inputs)
 
-    self.assertLen(variables, 1)
-    self.assertLen(variables['params'], 4)
-    self.assertEqual(out.shape, (1, num_classes))
+        self.assertLen(variables, 1)
+        self.assertLen(variables["params"], 4)
+        self.assertEqual(out.shape, (1, num_classes))
 
 
-if __name__ == '__main__':
-  absltest.main()
+if __name__ == "__main__":
+    absltest.main()
