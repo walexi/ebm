b069e0fae34e4a73190e82b90e203596108cacc3
Olawale Onabola <walexi4great@gmail.com>
2025-01-10 20:10:03 -0800

    updated

--- sampler.py
+++ sampler.py
@@ -6,7 +6,7 @@ from typing import Dict
 import random
 # @TODO add more algorithms
 
-algorithms = Enum("algorithm", [("LD", 1), ("HMC", 2)])
+algorithms = Enum("algorithm", [("LDMC", 1), ("HMC", 2)])
 
 """
     @brief: Langevin Dynamics MCMC
@@ -17,25 +17,20 @@ algorithms = Enum("algorithm", [("LD", 1), ("HMC", 2)])
     @param steps: int - number of steps to take in langevin dynamics
     @return: jax.Array - sampled data
 """
-def ld(inputs: jax.Array, model: nnx.Module, params: Dict, step_size: int, steps: int) -> jax.Array:
+def ldmc(inputs: jax.Array, model: nnx.Module, params: Dict, step_size: int, steps: int) -> jax.Array:
     key = jax.random.key(0)
     # iterate over steps, adding noise (drawn from a normal dist) to inputs
-    # pertubed = [inputs]
     for _ in range(steps):
         # inputs.__add__(noise).clip(-1, 1)
         # grad(E(x_k-1))
-        # primals, f_vjp = jax.vjp(lambda x, p: -model.apply(p, x), inputs, params)
         dx = jax.grad(lambda x, p: -model.apply(p, x).sum(), argnums=0)(inputs, params)
-        # dx, _ = f_vjp(jnp.ones_like(primals, dtype=jnp.float32))
         key, subkey = jax.random.split(key)
-        print(dx)
+        jnp.clip(dx, -0.03, 0.03) # clip gradients
         # x_k-1 - [step_size * grad(E(x_k-1))] + noise (std=0.05)
-        dx = dx.clip(-0.03, 0.03) # clip gradients
-        inputs = inputs + -step_size * dx + 0.05*jax.random.normal(subkey, inputs.shape)
-        inputs = inputs.clip(-1, 1) # for stability
+        inputs+=(-step_size * dx + 0.05*jax.random.normal(subkey, inputs.shape))
+        jnp.clip(inputs, -1, 1)
 
     return inputs
-
 
 def hmc() -> jax.Array:
     return []
@@ -45,18 +40,8 @@ class Sampler:
     sample_size: int
 
     @staticmethod
-    def sample_(
-        inputs: jax.Array,
-        model: nnx.Module,
-        params: Dict,
-        method: algorithms,
-        step_size: int,
-        steps: int,
-    ):
-        # set in eval mode
-        # model.eval()
-        # sample = None
-        if method == algorithms.LD:
-            return ld(inputs, model, params, step_size, steps)
+    def sample_(inputs: jax.Array, model: nnx.Module, params: Dict, method: algorithms, step_size: int, steps: int):
+        if method == algorithms.LDMC:
+            return ldmc(inputs, model, params, step_size, steps)
         elif method == algorithms.HMC:
             return hmc()
--- sampler_test.py
+++ sampler_test.py
@@ -8,7 +8,7 @@ from torchvision.datasets import MNIST
 import random
 
 jax.config.update("jax_disable_most_optimizations", True)
-logging.getLogger('jax._src.xla_bridge').addFilter(lambda _: False)
+logging.getLogger("jax._src.xla_bridge").addFilter(lambda _: False)
 
 
 class SamplerTest(parameterized.TestCase):
@@ -22,19 +22,19 @@ class SamplerTest(parameterized.TestCase):
         self.shape = (1, self.img_size, self.img_size, 1)
         self.model = create_model(self.img_size, self.num_classes)
         self.params = self.model.init(jax.random.key(0), jnp.ones(self.shape))
-    
+
     @parameterized.product(
-        method=[algorithms.LD], step_size=[10, 20, 50], steps=[100, 200]
+        method=[algorithms.LDMC], step_size=[10, 20, 50], steps=[100, 200]
     )
     def test_algos(self, method: algorithms, step_size: int, steps: int):
         """Test implemented sampling techniques."""
-
         example = jax.random.gumbel(jax.random.PRNGKey(0), self.shape)
-        sample = Sampler.sample_(example, self.model, self.params, method, step_size, steps)
+        sample = Sampler.sample_(
+            example, self.model, self.params, method, step_size, steps
+        )
         # @TODO add more tests
         self.assertEqual(sample.shape, example.shape)
-    
-    
+
 
 if __name__ == "__main__":
     absltest.main()
